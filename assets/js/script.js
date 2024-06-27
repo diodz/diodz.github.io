e58e433f7b7dab5ac5a5680e7676910f33c6a9bc
Diego A. Diaz <diegodiaz@uchicago.edu>
2024-06-27 02:34:43 +0000

    small

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const Navbar = () => {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <a href="/" className="flex-shrink-0">
-                <span className="text-xl font-bold">Your Name</span>
+                <span className="text-xl font-bold">Diego Díaz</span>
               </a>
             </div>
             <div className="hidden md:block">
